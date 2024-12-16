e01d1f55e32a9c04af46f92c7556875ce66d4953
aravind-virtualTE <cheralaaravind@virtualemployee.com>
2024-12-16 10:24:24 +0530

    added new components loc, loAuth, Soc, Sagrmt

--- src/pages/Company/HongKong/ServiceAgreement.tsx
+++ src/pages/Company/HongKong/ServiceAgreement.tsx
@@ -9,9 +9,13 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { useAtom } from "jotai";
-import { companyServiceAgreementConsentAtom,companyIncorporationAtom } from "@/lib/atom";
+import { companyServiceAgreementConsentAtom, companyIncorporationAtom } from "@/lib/atom";
 import { useToast } from '@/hooks/use-toast';
 import { useTranslation } from "react-i18next";
+import LetterOfConsent from './LetterOfConsent';
+import AppointmentLetter from './AppointmentCompSecretary';
+import AuthorizationDetails from './LetterOfAuthurisation';
+import SignificantControllerForm from './SignificantController';
 
 const ServiceAgreementDocument: React.FC = () => {
   const [, setServiceAgreement] = useAtom(companyServiceAgreementConsentAtom);
@@ -87,7 +91,8 @@ const ServiceAgreementDocument: React.FC = () => {
     { title: t("ServiceAgreementDocument.section10Title"), content: t("ServiceAgreementDocument.section10Content") },
     { title: t("ServiceAgreementDocument.section11Title"), content: t("ServiceAgreementDocument.section11Content") },
     { title: t("ServiceAgreementDocument.section12Title"), content: t("ServiceAgreementDocument.section12Content") },
-    { title: t("ServiceAgreementDocument.section13Title"), subsections: [
+    {
+      title: t("ServiceAgreementDocument.section13Title"), subsections: [
         {
           subtitle: t("ServiceAgreementDocument.section13_1"),
           content: t("ServiceAgreementDocument.section13_1Content")
@@ -101,7 +106,7 @@ const ServiceAgreementDocument: React.FC = () => {
   ];
 
   useEffect(() => {
-    if(finalForm.serviceAgreementConsent){
+    if (finalForm.serviceAgreementConsent) {
       setConsent(true)
     }
   }, [finalForm.serviceAgreementConsent])
@@ -126,7 +131,7 @@ const ServiceAgreementDocument: React.FC = () => {
   return (
     <div className="max-w-4xl mx-auto p-6 space-y-6">
       <h1 className="text-3xl font-bold text-center mb-6">
-      {t("ServiceAgreementDocument.title")}
+        {t("ServiceAgreementDocument.title")}
       </h1>
 
       <ScrollArea className="h-[600px] w-full rounded-md border p-4">
@@ -171,6 +176,10 @@ const ServiceAgreementDocument: React.FC = () => {
           ))}
         </Accordion>
       </ScrollArea>
+      <AppointmentLetter />
+      <LetterOfConsent />
+      <AuthorizationDetails />
+      <SignificantControllerForm />
 
       <div className="flex items-center mt-6">
         <Checkbox
