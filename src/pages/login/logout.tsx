ae8a7895cedb99ddd7dbc55f9fa8486c2be87315
aravind-virtualTE <cheralaaravind@virtualemployee.com>
2025-03-19 13:51:18 +0530

    added temp fix for client login issue

---
+++ src/pages/login/logout.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useResetAllForms } from "@/lib/atom";
+const Logout = () => {
+  const navigate = useNavigate();
+  const resetAllForms = useResetAllForms();
+  useEffect(() => {
+    const logout = async () => {
+      localStorage.setItem("isAuthenticated", "false");
+      localStorage.removeItem("token");
+      localStorage.removeItem("companyRecordId");
+      window.dispatchEvent(new Event("storage"));
+      resetAllForms();
+      navigate("/");
+    };
+
+    logout();
+  }, [navigate, resetAllForms]);
+
+  return <p>Logging out...</p>;
+};
+
+export default Logout;
