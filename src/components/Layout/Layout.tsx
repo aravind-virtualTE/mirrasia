08bd876b2d408170d552873e8c2eb056a686bed0
aravind-virtualTE <cheralaaravind@virtualemployee.com>
2025-04-08 17:09:16 +0530

    commented the zoho chat  and sorted the chat feature

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -10,7 +10,7 @@ import Navbar from './Navbar';
 import jwtDecode from 'jwt-decode';
 import { TokenData } from '@/middleware/ProtectedRoutes';
 import { useResetAllForms } from '@/lib/atom';
-// import HelpDesk from '../HelpDesk';
+import SocialMediaWidget from '../SocialMedia';
 
 const Layout: React.FC = () => {
     const [isCollapsed, setIsCollapsed] = useState(false)
@@ -172,7 +172,7 @@ const Layout: React.FC = () => {
                     </main>
                 </ResizablePanel>
             </ResizablePanelGroup>
-            {/* <HelpDesk /> */}
+            <SocialMediaWidget />
         </div>
     )
 }
