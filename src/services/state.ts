cdeb1f127bf9907c6ac14651d6c5412e53b7e81c
aravind-virtualTE <cheralaaravind@virtualemployee.com>
2025-04-10 14:08:42 +0530

    changes in the admin dashboard

--- src/services/state.ts
+++ src/services/state.ts
@@ -1,7 +1,7 @@
 // import { companyIncorporationAtom } from '@/lib/atom';
 import { atom } from 'jotai';
 
-type AnyObject = Record<string, unknown>;
+export type AnyObject = Record<string, unknown>;
 export const companyIncorporationList = atom<AnyObject[]>([]);
 
 export const selectedServicesAtom = atom<string[]>([]);
