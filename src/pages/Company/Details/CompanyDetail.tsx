bc39c82298d704ce9c4172216590fe18a687de4c
aravind-virtualTE <cheralaaravind@virtualemployee.com>
2025-12-17 17:55:57 +0530

    changes in the text

--- src/pages/Company/Details/CompanyDetail.tsx
+++ src/pages/Company/Details/CompanyDetail.tsx
@@ -9,7 +9,7 @@ import PPifCompDetail from "./PPifCompDetail";
 
 const CompanyDetail = () => {
     const { countryCode, id } = useParams();
-    // console.log("countryCode-->", countryCode, "id-->", id);
+    console.log("countryCode-->", countryCode, "id-->", id);
     // return (
     //     <HkCompdetail id={id ?? ""} />
     // );
@@ -28,6 +28,29 @@ const CompanyDetail = () => {
                 return <PaCompdetail id={id ?? ""} />;
             case "PPIF":
                 return <PPifCompDetail id={id ?? ""} />;
+            case "GB": // United Kingdom
+                return <p>In progress...</p>
+            case "SC": // Seychelles
+                return <p>In progress...</p>
+            case "CH": // Switzerland
+                return <p>In progress...</p>
+            case "MT": // Malta
+                return <p>In progress...</p>
+            case "MH": // Marshall Islands
+                return <p>In progress...</p>
+            case "KY": // Cayman Islands
+                return <p>In progress...</p>
+            case "BVI": // British Virgin Islands
+                return <p>In progress...</p>
+            case "BZ": // Belize
+                return <p>In progress...</p>
+            case "CW": // Curacao
+                return <p>In progress...</p>
+            case "EE": // Estonia
+                return <p>In progress...</p>
+            case "VC": // Saint Vincent and the Grenadines
+                return <p>In progress...</p>
+            
             default:
                 return <p> Not Found... </p>
         }
