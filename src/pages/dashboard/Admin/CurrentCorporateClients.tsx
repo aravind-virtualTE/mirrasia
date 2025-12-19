0d55fc1e20c6e4af17a42c2bf1e335a49eba0d3e
aravind-virtualTE <cheralaaravind@virtualemployee.com>
2025-12-19 20:19:33 +0530

    changes in the styling added filter and count of items

--- src/pages/dashboard/Admin/CurrentCorporateClients.tsx
+++ src/pages/dashboard/Admin/CurrentCorporateClients.tsx
@@ -5,8 +5,8 @@ import { useNavigate } from "react-router-dom"
 
 const CurrentCorporateClient: React.FC<{ data: companyTableData[]; count:number }> = ({ data, count }) => {
     const navigate = useNavigate()
-    // console.log("CurrentCorporateClient data", data.length, "count", count)
-    const dataCount = data.length + count;
+    console.log("CurrentCorporateClient data", data.length, "count", count)
+    const dataCount = data.length;
     return (
         <Card onClick={() => navigate("/current-corporate-client")} className="cursor-pointer hover:shadow-lg transition-shadow p-4">
             <div className="flex items-center justify-between">
